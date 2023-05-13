559064b203277fbac0523f86b45bcc88623512ca
marisdanne <maris.danne@gamil.com>
2023-05-13 14:04:16 +0300

    print komanda

---
+++ 1_diena/print.py
@@ -0,0 +1,34 @@
+print("Sveika, pasaule!")
+print()
+print("Šodien ir saulaina diena.")
+print()
+
+print("piens olas maize biezpiens")
+print("piens", "olas", "maize", "biezpiens")
+
+print("piens", "olas", "maize", "biezpiens", sep="*#*")
+print("piens", "olas", "maize", "biezpiens", end="-beigas-")
+print("Šī ir pēdējā rindiņa")
+
+print("piens", "olas", "maize", "biezpiens", sep="***", end="-beigas-")
+print()
+
+print("=============== 1. veids ===============================")
+print("Šodien\n gribas\n iet\n ārā\n pastaigāties.\n")
+print("Šodien ir Muzeju Nakts 2023")
+
+print("=============== 2. veids ===============================")
+print("Šodien\ngribas\niet\nārā\npastaigāties.")
+print("Šodien ir Muzeju Nakts 2023")
+
+print("=============== 3. veids ===============================")
+print("Šodien")
+print("gribas")
+print("iet")
+print("ārā")
+print("pastaigāties.")
+print("Šodien ir Muzeju Nakts 2023")
+
+print("=============== 4. veids ===============================")
+print("Šodien", "gribas", "iet", "ārā" "pastaigāties.", sep="\n\n", end="\n\n")
+print("Šodien ir Muzeju Nakts 2023")
