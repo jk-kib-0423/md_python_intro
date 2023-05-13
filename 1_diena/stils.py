c680966907d2da8efffe735f95af5d450227a1b3
marisdanne <maris.danne@gamil.com>
2023-05-13 16:02:06 +0300

    stils

---
+++ 1_diena/stils.py
@@ -0,0 +1,18 @@
+# Stila vadlīnijas šeit: https://peps.python.org/pep-0008/
+# - mainīgo nosaukumu jāveido tā, lai varētu saprast tā saturu izvairīties no nosaukumiem piem. x, y, z, a, b, c
+# - mainīgo nosaukumā var būt cipari, bet mainīgā nosaukums nevar sākties ar ciparu
+vards2 = "Pēteris"
+#3vards = "Miķelis" # šis nav pareizi
+# - mainīgā nosaukums jāraksta ar maziem burtiem, ja tas satur vairākus vārdus, tad atdalam tos ar _, vai pielietojam stilu camelCase
+
+camelCaseStyle = "stils"
+snake_case_style = "stils"
+noncasestyle = "nestils" # Nepareizi
+
+# - visi lielie burti tiek lietoti, ja veidojam konstantes
+GRAVITACIJAS_PAATRINAJUMS = 9.8
+
+# - nekādā gadījumā nelietojam garumzīmes un atstarpes
+# mans mainigais = 5 # Nepareizi
+# vārds = "Māris" # Nepareizi
+# print(vārds)
