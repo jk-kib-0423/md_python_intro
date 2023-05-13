0ad44d1e014ee3d9cd57852393e63f916213a43c
marisdanne <maris.danne@gamil.com>
2023-05-13 15:37:00 +0300

    mainigie

---
+++ 1_diena/mainigie.py
@@ -0,0 +1,45 @@
+# kastītes nosaukums - mainīga nosaukums (reģistr-jūtīgi)
+# kastītes veids - datu tips
+# kastītes saturs - mainīgā vērtība
+
+# vienādības zīme (=) ir piešķiršanas operators, kas piešķir vērtību kreisajai pusei no labās puses.
+uzvards = "Danne"
+
+print(uzvards)
+print("uzvards")
+
+uzvards = "Ozols"
+
+print(uzvards)
+
+print("=================================")
+
+print(type(uzvards)) # datu tips str - string, jeb simbolu virkne
+
+teikums = "Šī ir gara simbolu virkne."
+print(teikums)
+print(type(teikums))
+
+vertejums = 8
+print(vertejums)
+print(type(vertejums)) # datu tips int - integer, jeb vesels skaitlis
+
+skaits = "5"
+print(skaits)
+print(type(skaits))
+
+attalums = 5.7
+print(attalums)
+print(type(attalums)) # datu tips - float (f), jeb racionāls skaitlis
+
+brilles = True
+print(brilles)
+print(type(brilles)) # datu tips bool - boolean, var saturēt vērtības True vai False
+
+# Dažādi mainīgie
+tips = "a"
+Tips = "b"
+
+
+help("keywords") # Python rezervētie vārdi, kurus nevar izmantot, kā mainīgā nosaukumus
+
