151e3806155b0a3ef89f36faf801151bbc2a0ca7
marisdanne <maris.danne@gamil.com>
2023-05-27 12:42:36 +0300

    piemers 1

---
+++ 2_diena/piemers_1.py
@@ -0,0 +1,8 @@
+while True:
+    pin_kods = input("Ievadi PIN kodu:")
+    if pin_kods == "7777":
+        break
+    else:
+        print("Pin kods nav pareizs!")
+    
+print("Slepenais saturs, kas ir noslēpts!")
